ade76a8eb4635191d493879f94bcf7c493485255
Chris Baume <chris.baume@bbc.co.uk>
2015-10-27 15:16:57 +0000

    Added logo link in notifications

--- public/js/notification.js
+++ public/js/notification.js
@@ -9,7 +9,7 @@ define([], function()
   };
   var notify = function(message) {
     if (("Notification" in window) && Notification.permission === 'granted') {
-      return new Notification(message);
+      return new Notification(message, {icon: '/public/assets/discourse-icon.png'});
     }
   };
   return {
