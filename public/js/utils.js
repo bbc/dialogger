002b64776a054e7e3df9514a6cd30a85cf316400
Chris Baume <chris.baume@bbc.co.uk>
2015-12-08 11:13:49 +0000

    Added comments to util.js

--- public/js/utils.js
+++ public/js/utils.js
@@ -2,30 +2,45 @@ define([
   'jquery'
 ], function($)
 {
-  var transcriptToHTML = function(asset) {
+  // convert a transcript to HTML
+  var transcriptToHTML = function(asset)
+  {
     var html = '';
     var words = asset.transcript.words; 
     var segments = asset.segments.segments;
     var currentSegment = 0;
+
+    // for each word
     for (var i=0; i<words.length; i++) {
+
+      // if there is a change in speaker
       if (currentSegment < segments.length &&
           words[i].start > segments[currentSegment].start) {
+
+        // start a new paragraph
         var speaker = segments[currentSegment].speaker['@id'];
         var gender = segments[currentSegment].speaker.gender;
         if (currentSegment>0) html += '</p>';
         html += '<p><span class="speaker '+gender+'">['+speaker+']</span> ';
         currentSegment += 1;
       }
+
+      // note word and its start and end times (in millisecs)
       var word = words[i].word;
       var startTime = words[i].start * 1000;
       var endTime = words[i].end * 1000;
+
+      // note start time of next word (for detecting cuts)
       if (i+1 >= words.length)
         var nextTime = startTime+endTime;
       else
         var nextTime = words[i+1].start * 1000;
+
+      // if word confidence is low, class as unsure
       var confidence = '';
       if (words[i].confidence && words[i].confidence < 0.8)
           confidence = ' class="unsure"';
+
       html += '<a data-start="'+startTime+
                '" data-end="'+endTime+
                '" data-next="'+nextTime+'"'+
@@ -34,6 +49,8 @@ define([
     html += '</p>';
     return html;
   };
+
+  // convert HTML into a transcript object
   var HTMLtoWords = function(html) {
     var words = [];
     $(html).find('a').not('.hidden a').each(function() {
@@ -45,6 +62,8 @@ define([
     });
     return words;
   };
+
+  // convert a transcript object to a list of inpoints and outpoints
   var wordsToEDL = function(words) {
     var edl=[];
     var inpoint = -1;
@@ -58,6 +77,9 @@ define([
     if (inpoint) edl.push([inpoint, words[words.length-1].end]);
     return edl;
   };
+
+  // convert a list of inpoints and outpoints to a playlist for
+  // html5-video-compositor
   var edlToPlaylist = function(edl, src) {
     var playlist = [];
     var time = 0;
