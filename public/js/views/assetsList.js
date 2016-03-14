61de8a5e2ca5b7dbcee3524309ce70a80e08dc70
Chris Baume <chris.baume@bbc.co.uk>
2016-03-14 11:40:44 +0000

    Revert "Removed pen modifications after merge"
    This reverts commit 76563077d4935a1e156bf3522f0ae2226793ee70.

--- public/js/views/assetsList.js
+++ public/js/views/assetsList.js
@@ -59,6 +59,10 @@ define([
         });
       }
     },
+    print: function(id) {
+      $('#printForm').data('id', id);
+      $('#printModal').modal('show');
+    },
     destroy: function(id) {
       var collection = this.collection;
       var name = collection.get(id).attributes.name;
@@ -96,6 +100,7 @@ define([
           var id = $(this).closest('.asset').data('id');
           if (text==='Rename') view.rename(id);
           else if (text==='Delete') view.destroy(id);
+          else if (text==='Edit with digital pen') view.print(id);
           $(this).dropdown('hide');
         }
       });
