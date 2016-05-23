057a2d0501e8053bbeba6070380206c9133bcf06
Chris Baume <chris.baume@bbc.co.uk>
2016-05-23 12:10:44 +0000

    Added pre-populated asset and small tweaks

--- config/passport.js
+++ config/passport.js
@@ -27,8 +27,22 @@ module.exports = function(passport, db)
             log.error(err);
             done(err);
           } else {
-            log.info({user: user}, 'Created user');
-            done(null, user._id);
+            db.assets.findById('56b1d6b6aba63c1836383817', function(err, asset) {
+              if (err) {
+                log.error(err);
+              } else {
+                asset.owner = user._id;
+                delete asset._id;
+                db.assets.insert(asset, function(err, asset) {
+                  if (err) {
+                    log.error(err);
+                  } else {
+                    log.info({user: user}, 'Created user');
+                    done(null, user._id);
+                  }
+                });
+              }
+            });
           }
         });
       } else {
