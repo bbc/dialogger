72824a0a5b4ef4bfa945fb67260c7182114fd04c
Chris Baume <chris.baume@bbc.co.uk>
2016-06-02 15:01:32 +0000

    Added untested http byte request support

--- controllers/assets.js
+++ controllers/assets.js
@@ -241,7 +241,38 @@ exports.preview = function(req, res)
       log.error(err);
       res.status(500).send(err);
     } else {
-      res.sendFile(doc[0].previewPath);
+
+      var path = doc[0].previewPath;
+      var stat = fs.statSync(path);
+      var total = stat.size;
+
+      if (req.headers.range) {   // meaning client (browser) has moved the forward/back slider
+                                 // which has sent this request back to this server logic ... cool
+        var range = req.headers.range;
+        var parts = range.replace(/bytes=/, "").split("-");
+        var partialstart = parts[0];
+        var partialend = parts[1];
+
+        var start = parseInt(partialstart, 10);
+        var end = partialend ? parseInt(partialend, 10) : total-1;
+        var chunksize = (end-start)+1;
+        console.log('RANGE: ' + start + ' - ' + end + ' = ' + chunksize);
+
+        var file = fs.createReadStream(path, {start: start, end: end});
+        res.writeHead(206, {
+          'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
+          'Accept-Ranges': 'bytes',
+          'Content-Length': chunksize/*,
+          'Content-Type': 'video/mp4'*/
+        });
+        file.pipe(res);
+
+      } else {
+
+        console.log('ALL: ' + total);
+        res.sendFile(path);
+      }
+
     }
   });
 };
